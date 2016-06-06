bed8b30bc4281f355b32ba4c81a1b55107e13cc5
ChaosElephent <hixcwork@163.com>
2016-06-06 16:49:31 +0800

    finish

--- app/src/main/java/cn/yet/have/dont/udacityproject0/MainActivity.java
+++ app/src/main/java/cn/yet/have/dont/udacityproject0/MainActivity.java
@@ -2,6 +2,8 @@ package cn.yet.have.dont.udacityproject0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void onFirst(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "First Event Activated", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
+    public void onSecond(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Second Event Activated", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onThird(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Third Event Activated", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onForth(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Fourth Event Activated", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onFifth(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Fifth Event Activated", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void onSixth(View view) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Sixth Event Activated", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 }
